f56c639247afe0bb04886115015dca5d42688f92
ANDRIANAMBININA <zo.andrianambinina1@gmail.com>
2023-10-15 20:36:09 +0300

    premier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppClientComponent } from './client/app-client/app-client.component';
+import { DasboardClientComponent } from './client/dasboard-client/dasboard-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppClientComponent, children: [
+    { path: '', component: DasboardClientComponent },
+    //{ path: 'reports', component: ReportsComponent },
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppClientComponent } from './client/app-client/app-client.component';
+import { DasboardClientComponent } from './client/dasboard-client/dasboard-client.component';
+import { AppAdminComponent } from './admin/app-admin/app-admin.component';
+import { DasboardAdminComponent } from './admin/dasboard-admin/dasboard-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppClientComponent,
+    DasboardClientComponent,
+    AppAdminComponent,
+    DasboardAdminComponent
   ],
   imports: [
     BrowserModule,
